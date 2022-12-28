7e539a84397cd3316de38a4618c885b6ef11e33a
Sebastian-AM-Wobin <sebastian.mendez.wobin@outlook.com>
2022-12-27 20:49:38 -0600

    [ERR]  settlements_ids = fields.One2many

--- wobin_ant_liq/models/inheritances.py
+++ wobin_ant_liq/models/inheritances.py
@@ -10,7 +10,7 @@ class AccountPayment(models.Model):
 
     advances_ids    = fields.One2many('wobin.advances', 'payment_related_id',
                                       string='Anticipo')     
-    settlements_ids = fields.Many2one('wobin.settlements', 'payment_related_id',
+    settlements_ids = fields.One2many('wobin.settlements', 'payment_related_id',
                                       string='Liquidación')
 
     @api.model
@@ -35,24 +35,7 @@ class AccountMove(models.Model):
 
     comprobations_ids = fields.One2many('wobin.comprobations', 'acc_mov_related_id',
                                         string='Comprobación')     
-    settlement_id   = fields.Many2one('wobin.settlements', 
-                                      string='Liquidación')
-   
 
-    """
-    @api.model
-    def create(self, vals):
-        #Try to modify flow in order to upate state in possible settlement related:
-        res = super(AccountMove, self).create(vals)
-
-        #If a new record was created successfully and settlement related exists
-        #update that settlement in order to change its state to 'settled':
-        if res.settlements_ids:
-            for settlement in res.settlements_ids:
-                settlement.update({'state': 'ready'})        
-        return res
-    """
-    
 
 
 
