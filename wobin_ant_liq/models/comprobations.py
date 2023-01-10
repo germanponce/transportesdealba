f9f3af19ef9c2319a8457fc3e88b99fd5b719405
Sebastian-AM-Wobin <sebastian.mendez.wobin@outlook.com>
2023-01-10 14:42:40 -0600

    [ERR] 'comprobations_ids': [(4, self.id)],

--- wobin_ant_liq/models/comprobations.py
+++ wobin_ant_liq/models/comprobations.py
@@ -215,7 +215,7 @@ class WobinComprobations(models.Model):
         # Creation of Account Move |
         #°°°°°°°°°°°°°°°°°°°°°°°°°°°          
         account_move = {
-                'trips_acc_move_ids': [(4, self.id)],
+                'comprobations_ids': [(4, self.id)],
                 'ref': 'PROVISION',                
                 'journal_id': 70,  #70 ID for Journal of "Contabilidad B" in Transportes de Alba ['Sistema' Company]             
                } 
